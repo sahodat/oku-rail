84f6cab1b8e399c0eac908f995f499e3a3302b47
shino <shinoppa220@gmail.com>
2018-11-29 13:13:57 +0900

    registration_DB_jobans

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_060211) do
+ActiveRecord::Schema.define(version: 2018_11_29_033926) do
+
+  create_table "opdt_jobans", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "o_id"
+    t.string "o_type"
+    t.datetime "date"
+    t.string "context"
+    t.datetime "o_valid"
+    t.integer "delay"
+    t.integer "opdt_index"
+    t.string "same_as"
+    t.string "railway"
+    t.string "operator"
+    t.string "to_station"
+    t.string "train_type"
+    t.string "from_station"
+    t.string "train_number"
+    t.string "direction"
+    t.integer "composition"
+    t.string "destinantion_station"
+  end
 
   create_table "opdt_yamanotes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "o_id"
