23240d5d53d1f8298a783c1a0ebc5eadd508bef7
shino <shinoppa220@gmail.com>
2018-12-06 17:12:59 +0900

    Edit deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,35 @@
+# # config valid only for current version of Capistrano
+# lock '~> 3.11.0'
+
+# set :application, 'oku-rail'
+# set :repo_url,  'git@github.com:sahodat/oku-rail.git'
+
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+# set :rbenv_type, :user
+# set :rbenv_ruby, '2.3.1'
+
+# set :ssh_options, auth_methods: ['publickey'],
+#                   keys: ['/Users/tech-camp/.ssh/oku-rail.pem']
+
+# set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+# set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+# set :keep_releases, 5
+
+# after 'deploy:publishing', 'deploy:restart'
+# namespace :deploy do
+#   task :restart do
+#     invoke 'unicorn:restart'
+#   end
+# end
+
+
 # config valid only for current version of Capistrano
 lock '~> 3.11.0'
 
 set :application, 'oku-rail'
-set :repo_url,  'git@github.com:sahodat/oku-rail.git'
+set :repo_url,  'git@github.com:sahodat/oku-rail.git
+'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
