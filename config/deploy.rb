dd14e89e55c1bd942524b96c3afe2d05a87e5ad1
shino <shinoppa220@gmail.com>
2018-12-06 16:08:57 +0900

    Edit deploy,rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '~> 3.11.0'
 
 set :application, 'oku-rail'
-set :repo_url,  'git@github.com:sahodat/oku-rail.git.git'
+set :repo_url,  'git@github.com:sahodat/oku-rail.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
