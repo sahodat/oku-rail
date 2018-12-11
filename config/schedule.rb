c3e353fe6b5b5629793f4ff4be05789afaef825d
shino <shinoppa220@gmail.com>
2018-12-11 18:45:40 +0900

    edit2 whenever/schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -25,7 +25,7 @@
 
 # Learn more: http://github.com/javan/whenever
 
-every 30.minutes do
+every 10.minutes do
   runner 'Scraping_j.get_opdt_joban'
   runner 'Scraping_j2.get_opdt_joban'
   runner 'Scraping_j3.get_opdt_joban'
