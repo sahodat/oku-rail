6582396e9d2b0a6b68cae130a89274812ba6e1f1
sahodat <7207t251s906@gmail.com>
2018-12-11 19:31:57 +0900

    backbutton

--- app/controllers/delay_controller.rb
+++ app/controllers/delay_controller.rb
@@ -1,4 +1,13 @@
 class DelayController < ApplicationController
+
 def delay
+
+
+    @jobancount = OpdtJoban.group("HOUR(date)").count
+    @jobandelay0 = OpdtJoban.group("HOUR(date)").where("(delay = 0)").count
+    @smalldelay = OpdtJoban.group("HOUR(date)").where("delay > 0 and delay < 301").count
+    @hugedelay = OpdtJoban.group("HOUR(date)").where("delay > 900").count
+
 end
+
 end
