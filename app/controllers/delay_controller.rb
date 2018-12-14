27c0aba8cc98091bafd29c7991c0700f68061226
sahodat <7207t251s906@gmail.com>
2018-12-14 17:31:51 +0900

    get_time&station_record

--- app/controllers/delay_controller.rb
+++ app/controllers/delay_controller.rb
@@ -2,11 +2,22 @@ class DelayController < ApplicationController
 
 def delay
 
+    @jcs = OpdtJoban.group("HOUR(date)").group(:from_station).count
+    @jcssmalldelay = OpdtJoban.group("HOUR(date)").group(:from_station).where("delay > 0 and delay < 301").count
+    @jcshugedelay = OpdtJoban.group("HOUR(date)").group(:from_station).where("delay > 900").count
+    gon.job = @jcssmalldelay
+    gon.job2 =  @jcs
+    gon.job3 = @jcshugedelay
 
-    @jobancount = OpdtJoban.group("HOUR(date)").count
-    @jobandelay0 = OpdtJoban.group("HOUR(date)").where("(delay = 0)").count
-    @smalldelay = OpdtJoban.group("HOUR(date)").where("delay > 0 and delay < 301").count
-    @hugedelay = OpdtJoban.group("HOUR(date)").where("delay > 900").count
+
+    @ys = OpdtYamanote.group("HOUR(date)").group(:from_station).count
+    @yssmalldelay = OpdtYamanote.group("HOUR(date)").group(:from_station).where("delay > 0 and delay < 301").count
+    @yshugedelay = OpdtYamanote.group("HOUR(date)").group(:from_station).where("delay > 900").count
+    gon.yam = @yssmalldelay
+    gon.yam2 =  @ys
+    gon.yam3 = @yshugedelay
+
+
 
 end
 
