553338c243afeee20cd876d89e96f1fdf40253c3
shino <shinoppa220@gmail.com>
2018-12-18 19:04:05 +0900

    add_routes3

--- app/views/delay2/site.js
+++ app/views/delay2/site.js
@@ -1,35 +1,35 @@
-$(function(){
-  var DATA = []
-  var data = ["ueno-tokyo", "tokyo-shinagwa", "shinagwa-shibuya","shibuya-shinjyuku","shinjyuku-ikebukuro","ikebukuro-ueno"];
-for(var i=0;i<=5;i++) {
-    DATA.push({
-      label: data[i],
-      url: 'menu-'+i
-    });
-  }
+// $(function(){
+//   var DATA = []
+//   var data = ["ueno-tokyo", "tokyo-shinagwa", "shinagwa-shibuya","shibuya-shinjyuku","shinjyuku-ikebukuro","ikebukuro-ueno"];
+// for(var i=0;i<=5;i++) {
+//     DATA.push({
+//       label: data[i],
+//       url: 'menu-'+i
+//     });
+//   }
 
-  var SELECTED = 'menu-0';
+//   var SELECTED = 'menu-0';
 
-  var OPTIONS = {
-    key: 'url',
-    pageLoader: {
-      target: null,
-      key: 'url'
-    }
-  }
+//   var OPTIONS = {
+//     key: 'url',
+//     pageLoader: {
+//       target: null,
+//       key: 'url'
+//     }
+//   }
 
-  var FUNC = {
-    onInit: function() {
-      console.log('INIT');
-    },
-    onChangeBegin: function() {
-      console.log('CHANGE');
-    },
-    onChangeComplete: function(d) {
-      console.log('CHANGE_COMPLETE')
-      console.log(d)
-    }
-  };
+//   var FUNC = {
+//     onInit: function() {
+//       console.log('INIT');
+//     },
+//     onChangeBegin: function() {
+//       console.log('CHANGE');
+//     },
+//     onChangeComplete: function(d) {
+//       console.log('CHANGE_COMPLETE')
+//       console.log(d)
+//     }
+//   };
 
-  var circleMenu = new CircleMenu($('#my-circle-menu'), DATA, SELECTED, OPTIONS, FUNC);
-});
+//   var circleMenu = new CircleMenu($('#my-circle-menu'), DATA, SELECTED, OPTIONS, FUNC);
+// });
