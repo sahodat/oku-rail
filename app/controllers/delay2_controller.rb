d705db29240b7d1dd9bd1ae4395acdf737a23468
shino <shinoppa220@gmail.com>
2018-12-18 18:37:23 +0900

    delay2_view

---
+++ app/controllers/delay2_controller.rb
@@ -0,0 +1,13 @@
+class Delay2Controller < ApplicationController
+
+def delay2
+
+
+    # @jobancount = OpdtJoban.group("HOUR(date)").count
+    # @jobandelay0 = OpdtJoban.group("HOUR(date)").where("(delay = 0)").count
+    # @smalldelay = OpdtJoban.group("HOUR(date)").where("delay > 0 and delay < 301").count
+    # @hugedelay = OpdtJoban.group("HOUR(date)").where("delay > 900").count
+
+end
+
+end
