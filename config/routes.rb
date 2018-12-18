fc2e5c044afd23659e1c27e8ac83ab94e07b390f
shino <shinoppa220@gmail.com>
2018-12-18 19:10:44 +0900

    test_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   root "index#index"
+   root "delay2#delay2'
+"
 
    get   'delay'      =>  'delay2#delay2'
    get   'delay2'    =>  'delay#delay'
