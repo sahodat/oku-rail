92a9ed284e93fee5ff60e718a7a688dde89f3b69
shino <shinoppa220@gmail.com>
2018-12-18 18:28:23 +0900

    set_delay2_view

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root "index#index"
 
-   get   'delay'      =>  'delay#delay'
+   get   'delay'      =>  'delay2#delay2'
+   get   'delay2'    =>  'delay2#delay2'
 end
