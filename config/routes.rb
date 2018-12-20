3d43ab4a14297ae946ee6fb0e4b364509e860450
shino <shinoppa220@gmail.com>
2018-12-20 16:04:29 +0900

    routiggroup

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
    root "index#index"
 
    get   'delay'      =>  'delay2#delay2'
+   get   'delay201'      =>  'delay201#delay201'
+   get   'delay202'      =>  'delay202#delay202'
+   get   'delay203'      =>  'delay202#delay202'
+   get   'delay204'      =>  'delay203#delay203'
+   get   'delay205'      =>  'delay203#delay205'
    get   'delay2'    =>  'delay#delay'
 end
