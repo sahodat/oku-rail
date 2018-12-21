e78f32358fee5c8f2a82cfbac5093d9bdba0bbd5
sahodat <7207t251s906@gmail.com>
2018-12-21 18:41:04 +0900

    cal_joban_percent

--- app/controllers/delay_joban_controller.rb
+++ app/controllers/delay_joban_controller.rb
@@ -2,20 +2,22 @@ class DelayJobanController < ApplicationController
 
   def delay_joban
 
-    @jcs = OpdtJoban.group("HOUR(date)").group(:from_station).count
-    @jcssmalldelay = OpdtJoban.group("HOUR(date)").group(:from_station).where("delay > 0 and delay < 301").count
-    @jcshugedelay = OpdtJoban.group("HOUR(date)").group(:from_station).where("delay > 900").count
-    gon.job = @jcssmalldelay
-    gon.job2 =  @jcs
-    gon.job3 = @jcshugedelay
+@jobtest = OpdtJoban.group("HOUR(date)").group(:from_station).group(:direction).count
+@jobtestdelay = OpdtJoban.group("HOUR(date)").group(:from_station).group(:direction).where("delay > 0 and delay < 301").count
+@jobtesthugedelay = OpdtJoban.group("HOUR(date)").group(:from_station).where("delay > 1200").count
+
+    gon.jobtest = @jobtest
+    gon.jobtestdelay = @jobtestdelay
+    gon.jobtesthugedelay =@jobtesthugedelay
 
 
-    @ys = OpdtYamanote.group("HOUR(date)").group(:from_station).count
-    @yssmalldelay = OpdtYamanote.group("HOUR(date)").group(:from_station).where("delay > 0 and delay < 301").count
-    @yshugedelay = OpdtYamanote.group("HOUR(date)").group(:from_station).where("delay > 900").count
-    gon.yam = @yssmalldelay
-    gon.yam2 =  @ys
-    gon.yam3 = @yshugedelay
+@yamtest = OpdtYamanote.group("HOUR(date)").group(:from_station).group(:direction).count
+@yamtestdelay = OpdtYamanote.group("HOUR(date)").group(:from_station).group(:direction).where("delay > 59 and delay < 301").count
+@yamtesthugedelay = OpdtYamanote.group("HOUR(date)").group(:from_station).group(:direction).where("delay > 901").count
+
+    gon.yamtest = @yamtest
+    gon.yamtestdelay = @yamtestdelay
+    gon.yamatesthugedelay = @yamtesthugedelay
 
 
 end
