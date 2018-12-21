d0c59776e9d70784f21496e4328c9cc0eb12af71
sahodat <7207t251s906@gmail.com>
2018-12-21 19:25:15 +0900

    cal_yamate_percent

--- app/controllers/delay2_controller.rb
+++ app/controllers/delay2_controller.rb
@@ -2,20 +2,13 @@ class Delay2Controller < ApplicationController
 
 def delay2
 
-    @jcs = OpdtJoban.group("HOUR(date)").group(:from_station).count
-    @jcssmalldelay = OpdtJoban.group("HOUR(date)").group(:from_station).where("delay > 0 and delay < 301").count
-    @jcshugedelay = OpdtJoban.group("HOUR(date)").group(:from_station).where("delay > 900").count
-    gon.job = @jcssmalldelay
-    gon.job2 =  @jcs
-    gon.job3 = @jcshugedelay
+@yamtest = OpdtYamanote.group("HOUR(date)").group(:from_station).group(:direction).count
+@yamtestdelay = OpdtYamanote.group("HOUR(date)").group(:from_station).group(:direction).where("delay > 59 and delay < 301").count
+@yamtesthugedelay = OpdtYamanote.group("HOUR(date)").group(:from_station).group(:direction).where("delay > 901").count
 
-
-    @ys = OpdtYamanote.group("HOUR(date)").group(:from_station).count
-    @yssmalldelay = OpdtYamanote.group("HOUR(date)").group(:from_station).where("delay > 0 and delay < 301").count
-    @yshugedelay = OpdtYamanote.group("HOUR(date)").group(:from_station).where("delay > 900").count
-    gon.yam = @yssmalldelay
-    gon.yam2 =  @ys
-    gon.yam3 = @yshugedelay
+    gon.yamtest = @yamtest
+    gon.yamtestdelay = @yamtestdelay
+    gon.yamatesthugedelay = @yamtesthugedelay
 
 
   # 天気の表示をさせたかった
