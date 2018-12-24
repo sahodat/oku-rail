b776e58e4a9d0ebab8f0eaadef3a7444586dec5e
sahodat <7207t251s906@gmail.com>
2018-12-25 03:28:30 +0900

    indent

--- app/controllers/delay2_controller.rb
+++ app/controllers/delay2_controller.rb
@@ -1,10 +1,10 @@
 class Delay2Controller < ApplicationController
 
-def delay2
+  def delay2
 
-@yam = OpdtYamanote.group("HOUR(date)").group(:from_station).group(:direction).count
-@yamdelay = OpdtYamanote.group("HOUR(date)").group(:from_station).group(:direction).where("delay > 59 and delay < 301").count
-@yamhugedelay = OpdtYamanote.group("HOUR(date)").group(:from_station).group(:direction).where("delay > 901").count
+    @yam = OpdtYamanote.group("HOUR(date)").group(:from_station).group(:direction).count
+    @yamdelay = OpdtYamanote.group("HOUR(date)").group(:from_station).group(:direction).where("delay > 59 and delay < 301").count
+    @yamhugedelay = OpdtYamanote.group("HOUR(date)").group(:from_station).group(:direction).where("delay > 901").count
 
     gon.yam = @yam
     gon.yamdelay = @yamdelay
@@ -23,6 +23,32 @@ def delay2
   #   @wether = JSON.pretty_generate(JSON.parse(response.read))
   #   gon.wether = @wether
 
-end
+# # 天気うまくいないなああ
+  # def weather
+  #   require 'net/http'
+  #   require 'uri'
+  #   require 'json'
+
+  #   uri = URI.parse('http://weather.livedoor.com/forecast/webservice/json/v1?city=130010')
+  #   json = Net::HTTP.get(uri)
+  #   result = JSON.parse(json)
+
+
+  #   gon.weather = "aa"
+
+  #   print result['title'], "\n"
+  #   print result['link'], "\n"
+  #   print "予報の発表日時:#{result['publicTime']}", "\n"
+  #   result['forecasts'].each do |forecast|
+  #   print "--------------------"
+  #   print "予報日:#{forecast['dateLabel']}", "\n"
+  #   print "天気:#{forecast['telop']}", "\n"
+  #   print "最低気温:#{forecast['temperature']['min']}", "\n"
+  #   print "最高気温:#{forecast['temperature']['max']}", "\n"
+  #   end
+  # end
+
+
+  end
 
 end
--- app/controllers/delay_joban_controller.rb
+++ app/controllers/delay_joban_controller.rb
@@ -2,9 +2,9 @@ class DelayJobanController < ApplicationController
 
   def delay_joban
 
-@job = OpdtJoban.group("HOUR(date)").group(:from_station).group(:direction).count
-@jobdelay = OpdtJoban.group("HOUR(date)").group(:from_station).group(:direction).where("delay > 0 and delay < 301").count
-@jobhugedelay = OpdtJoban.group("HOUR(date)").group(:from_station).where("delay > 900").count
+    @job = OpdtJoban.group("HOUR(date)").group(:from_station).group(:direction).count
+    @jobdelay = OpdtJoban.group("HOUR(date)").group(:from_station).group(:direction).where("delay > 0 and delay < 301").count
+    @jobhugedelay = OpdtJoban.group("HOUR(date)").group(:from_station).where("delay > 900").count
 
     gon.job = @job
     gon.jobdelay = @jobdelay
